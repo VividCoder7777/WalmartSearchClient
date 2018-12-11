c8def6b0f2a6f8255d24bcb4a0a0bf20402feb70
Raymond <raymond.yu171@gmail.com>
2018-12-11 17:04:47 -0500

    'script'

--- sw.js
+++ sw.js
@@ -4,7 +4,11 @@ workbox.clientsClaim();
 workbox.precaching.precacheAndRoute([]);
 
 workbox.precaching.suppressWarnings();
-workbox.routing.registerRoute('https://walmartsearch.herokuapp.com/walmart/product', workbox.strategies.cacheFirst());
+workbox.routing.registerRoute(
+	new RegExp('https://walmartsearch.herokuapp.com/walmart/product/'),
+	workbox.strategies.cacheFirst()
+);
+
 workbox.routing.registerNavigationRoute('/index.html', {
 	blacklist: [ /^\/_/, /\/[^\/]+\.[^\/]+$/ ]
 });
